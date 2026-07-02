e150a9998d2a7df1cf7455dc68d6bb528c044f6b
TANVIR0222 <tanvirislam3912@gmail.com>
2026-07-02 17:11:01 +0600

    feat: implement dynamic ScrollToTop component and update layout structure

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,6 +1,12 @@
-import { AppFooter } from "@/components/app-footer"
+import dynamic from "next/dynamic"
+
 import { SiteBottomNav } from "@/components/site-bottom-nav"
-import React from "react"
+import { SiteFooter } from "@/components/site-footer"
+import { SiteHeader } from "@/components/site-header"
+
+const ScrollToTop = dynamic(() =>
+  import("@/components/scroll-to-top").then((mod) => mod.ScrollToTop)
+)
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -10,6 +16,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
     <div className="group/layout relative isolate">
       {/* <SiteHeader /> */}
       <main className="max-w-screen overflow-x-clip px-2">{children}</main>
+      <SiteFooter />
       <div
         className="pointer-events-none fixed inset-x-0 bottom-0 z-50"
         aria-hidden
@@ -17,8 +24,8 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
         <div className="h-(--fade-bottom-height) bg-linear-to-b from-transparent to-background mask-linear-[to_top,var(--background)_25%,transparent] backdrop-blur-[1px]" />
         <div className="bg-background pb-[env(safe-area-inset-bottom,0)]" />
       </div>
-      <SiteBottomNav />
-      {/* <AppFooter /> */}
+      {/* <SiteBottomNav /> */}
+      <ScrollToTop />
     </div>
   )
 }
