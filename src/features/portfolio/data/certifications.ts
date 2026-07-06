3ca8c8c79bd0480ffab231fc971ade860cb6f72d
TANVIR0222 <tanvirislam3912@gmail.com>
2026-07-06 17:14:34 +0600

    fix: update credentialID and URL for Full Stack Web Development certification

--- src/features/portfolio/data/certifications.ts
+++ src/features/portfolio/data/certifications.ts
@@ -5,9 +5,9 @@ export const CERTIFICATIONS: Certification[] = [
     title: " Full Stack Web Development Cohort 2026",
     issuer: "chai aur code",
     issueDate: "2026-07-01",
-    credentialID: "594593",
+    credentialID: "11502636-250927",
     credentialURL:
-      "https://drive.google.com/file/d/1XrT62TF4FjItkT2TVq29OzJRm32Bcwfk/view?usp=sharing",
+      "https://storage.googleapis.com/learnyst-user-assets/certificates/schools/171024/certificates/250927/users/11502636/pdf/11502636_250927.pdf",
   },
   {
     title: " Mobile Development Cohort",
